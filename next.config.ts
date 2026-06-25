d45fd64145fa28e70569b2126575f59925807468
kalandhar01 <kalandars2004@gmail.com>
2026-06-25 12:11:23 +0530

    fix: Cloudinary remotePatterns update

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,16 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   devIndicators: false,
   allowedDevOrigins: ["10.54.155.196", "10.185.136.238", "10.169.138.244", "10.48.213.238"],
+  images: {
+    formats: ["image/avif", "image/webp"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "/dgqbgdk73/**"
+      }
+    ]
+  }
 };
 
 export default nextConfig;
